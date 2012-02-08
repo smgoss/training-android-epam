ad51d0047127a22f6c00ebdc3ab44cff1138cfbe
agidel2012 <agidel2012@258f82a4-f076-c894-e823-74c26278488b>
2012-02-08 11:39:58 +0000

    Added: - AbstractAdapter - ArrayModelListAdapter - loadarraymodel layout - loadmodel layout
    Updated:
    - values\strings 
    - main layout
    - android manifest
    - mainActivity

---
+++ trunk/SocialLibrary/src/com/epam/android/social/LoadModelActivity.java
@@ -0,0 +1,14 @@
+package com.epam.android.social;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class LoadModelActivity extends Activity {
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.loadmodel);
+	        Toast.makeText(this, "TODO LoadModel undone!", Toast.LENGTH_SHORT).show(); 
+	        //TODO Load username and avatar
+	    }
+}
--- trunk/SocialLibrary/src/com/epam/android/social/MainActivity.java
+++ trunk/SocialLibrary/src/com/epam/android/social/MainActivity.java
@@ -1,13 +1,25 @@
 package com.epam.android.social;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	public void onModelButtonClick(View view) {
+		Intent intent = new Intent(this, LoadModelActivity.class);
+		startActivity(intent);
+	}
+	
+	public void onArrayModelButtonClick(View view) {
+		Intent intent = new Intent(this, LoadArrayModelActivity.class);
+		startActivity(intent);
+	}
 }
