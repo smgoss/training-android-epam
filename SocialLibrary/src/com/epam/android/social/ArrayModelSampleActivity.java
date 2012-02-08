717d9a8cfe593d6a2c389a44bd5a9db0cd080d59
agidel2012 <agidel2012@258f82a4-f076-c894-e823-74c26278488b>
2012-02-08 15:49:42 +0000

    some refactoring

--- SocialLibrary/src/com/epam/android/social/ArrayModelSampleActivity.java
+++ SocialLibrary/src/com/epam/android/social/ArrayModelSampleActivity.java
@@ -5,24 +5,22 @@ import com.epam.android.common.adapter.ArrayModelListAdapter;
 import com.epam.android.common.model.BaseModel;
 import com.epam.android.common.task.LoadModelAsyncTask;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.Toast;
-//TODO rename array model sample activity
-public class LoadArrayModelActivity extends DelegateActivity {
+
+public class ArrayModelSampleActivity extends DelegateActivity {
 
 	private ListView mListView;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//TODO put _
-		setContentView(R.layout.loadarraymodel);
+		setContentView(R.layout.load_array_model);
 		Toast.makeText(this, "TODO LoadArrayModel undone!", Toast.LENGTH_SHORT)
 				.show();
 		// TODO Load username and avatar
 
-		mListView = (ListView) findViewById(R.id.arraymodellist);
+		mListView = (ListView) findViewById(R.id.array_model_list);
 		
 		
 		 //setListAdapter(new ArrayModelListAdapter(
--- SocialLibrary/src/com/epam/android/social/DelegateActivity.java
+++ SocialLibrary/src/com/epam/android/social/DelegateActivity.java
@@ -6,23 +6,40 @@ import com.epam.android.common.task.CommonAsyncTask;
 import com.epam.android.common.task.IDelegate;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 public class DelegateActivity extends Activity implements IDelegate {
 
+	private static final String TAG = DelegateActivity.class.getSimpleName();
+
+	private static final String TITLE = "Basic title";
+
+	private static final String MSG = "Basic message";
+
+	private ProgressDialog mProgressDialog;
+
 	public void showloading() {
-		// TODO show loading dialog
-		
+		if (mProgressDialog == null) {
+			mProgressDialog = ProgressDialog.show(getContext(), TITLE, MSG,
+					false, true);
+		} else {
+			mProgressDialog.show();
+		}
 	}
 
 	public void hideloading() {
-		// TODO hide loading dialog
-		
+		if (mProgressDialog.isShowing()) {
+			mProgressDialog.dismiss();
+		}
 	}
 
 	public void handleError(CommonAsyncTask task, IOException e) {
-		// TODO show toast message with error
-		
+		Log.e(TAG, "http client err: " + e.getMessage(), e);
+		Toast.makeText(getContext(), "http client err: " + e.getMessage(),
+				Toast.LENGTH_SHORT).show();
 	}
 
 	public Context getContext() {
@@ -31,12 +48,12 @@ public class DelegateActivity extends Activity implements IDelegate {
 
 	public void addTask(CommonAsyncTask task) {
 		// TODO list storage of task if task started
-		
+
 	}
 
 	public void removeTask(CommonAsyncTask task) {
 		// TODO remove task if task ends or canceled
-		
+
 	}
 
 }
