b6e182762dfa1ad69b1fc0181e02f8bafbdce965
agidel2012 <agidel2012@258f82a4-f076-c894-e823-74c26278488b>
2012-02-08 09:42:02 +0000

    added CommonAsynkTask class

---
+++ SocialLibrary/src/com/epam/android/common/task/CommonIDelegate.java
@@ -0,0 +1,46 @@
+package com.epam.android.common.task;
+
+//import java.io.IOException;
+//
+//import android.app.ProgressDialog;
+//import android.content.Context;
+//import android.util.Log;
+//import android.widget.Toast;
+
+public class CommonIDelegate {
+	
+//	private static final String TITLE = "Basic title";
+//
+//	private static final String MSG = "Basic message";
+//
+//	private ProgressDialog mProgressDialog;
+//
+//	@Override
+//	public void showloading() {
+//		if (mProgressDialog == null) {
+//			mProgressDialog = ProgressDialog.show(getContext(), TITLE, MSG,
+//					false, true);
+//		} else {
+//			mProgressDialog.show();
+//		}
+//	}
+//
+//	@Override
+//	public void hideloading() {
+//		if (mProgressDialog.isShowing()) {
+//			mProgressDialog.dismiss();
+//		}
+//	}
+//
+//	@Override
+//	public void handleError(IOException e) {
+//		Log.e(TAG, "http client err: " + e.getMessage(), e);
+//		Toast.makeText(getContext(), "http client err: " + e.getMessage(),
+//				Toast.LENGTH_SHORT).show();
+//	}
+//
+//	@Override
+//	public Context getContext() {
+//		return this.getContext();
+//	}
+}
