de5136bb1a2f6a6738fb9c7f213eb9b9bcfe3a12
agidel2012 <agidel2012@258f82a4-f076-c894-e823-74c26278488b>
2012-02-09 09:44:15 +0000

    update: - ModelSampleActivity - CommonActiveTask - LoadModelAsyncTask

--- SocialLibrary/src/com/epam/android/common/task/LoadArrayModelAsyncTask.java
+++ SocialLibrary/src/com/epam/android/common/task/LoadArrayModelAsyncTask.java
@@ -3,24 +3,20 @@ package com.epam.android.common.task;
 import java.io.IOException;
 import java.util.List;
 
-public abstract class LoadArrayModelAsyncTask <B> extends CommonAsyncTask<List<B>>  {
-	
+public abstract class LoadArrayModelAsyncTask<B> extends
+		CommonAsyncTask<List<B>> {
+
 	public LoadArrayModelAsyncTask(String url, IDelegate delegate) {
 		super(url, delegate);
 	}
 
-
 	@Override
-	public List<B> load(String url) throws IOException {
-		// TODO Load json to model 
+	public List<B> load() throws IOException {
+		// TODO Load json to model
 		return null;
 	}
 
-
 	@Override
 	public abstract void success(List<B> result);
-		
-	
-}
-	
 
+}
