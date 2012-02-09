805e9346277d0fd743251133ca2f792d43d99a02
ilya.shknaj <ilya.shknaj@258f82a4-f076-c894-e823-74c26278488b>
2012-02-09 10:04:50 +0000

    added List<User>

--- SocialLibrary/src/com/epam/android/social/MainActivity.java
+++ SocialLibrary/src/com/epam/android/social/MainActivity.java
@@ -1,8 +1,18 @@
 package com.epam.android.social;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+
+import com.epam.android.common.model.Loader;
+import com.epam.android.common.model.User;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -11,6 +21,21 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		Loader loader = new Loader(this);
+		try {
+			List<User> userList= loader.loadArrayModel("http://dl.dropbox.com/u/16403954/array_bm.json", User.MODEL_CREATOR);
+			Log.d("TAG", userList.get(1).getName());
+			Log.d("TAG", userList.get(0).getImageUrl());
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void onModelButtonClick(View view) {
