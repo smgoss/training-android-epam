fdcefd1ca474d0a58326264c0803246fab6cc3ca
agidel2012 <agidel2012@258f82a4-f076-c894-e823-74c26278488b>
2012-02-09 08:34:05 +0000

    updated ModelSampleActivity (imageloader)

--- trunk/SocialLibrary/src/com/epam/android/social/ModelSampleActivity.java
+++ trunk/SocialLibrary/src/com/epam/android/social/ModelSampleActivity.java
@@ -1,25 +1,37 @@
 package com.epam.android.social;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.epam.android.common.model.User;
 import com.epam.android.common.task.LoadModelAsyncTask;
+import com.google.android.imageloader.ImageLoader;
 
 public class ModelSampleActivity extends DelegateActivity {
 
+	protected ImageLoader mImageLoader;
+	
+	public static final String URL = "http://dl.dropbox.com/u/16403954/bm.json";
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.load_model);
-		Toast.makeText(this, "TODO LoadModel undone!", Toast.LENGTH_SHORT)
-				.show();
-		// TODO Load username and avatar
 		
-		new LoadModelAsyncTask<User>("url", this, User.MODEL_CREATOR) {
+		final TextView userName = (TextView) findViewById(R.id.userName);
+		final ImageView userAvatar = (ImageView) findViewById(R.id.userAvatar);
+		
+//		Toast.makeText(this, "TODO LoadModel undone!", Toast.LENGTH_SHORT)
+//				.show();
+//			
+		new LoadModelAsyncTask<User>(URL, this, User.MODEL_CREATOR) {
 
 			@Override
 			public void success(User result) {
-				// TODO if success
+				userName.setText(result.getName());
+				mImageLoader = ImageLoader.get(ModelSampleActivity.this);
+				mImageLoader.bind(userAvatar, result.getImageUrl(), null);
 				
 			}
 		};
--- trunk/SocialLibrary/src/com/epam/android/social/SocialApplication.java
+++ trunk/SocialLibrary/src/com/epam/android/social/SocialApplication.java
@@ -1,16 +1,19 @@
 package com.epam.android.social;
 
 import com.epam.android.common.http.HttpClient;
+import com.google.android.imageloader.ImageLoader;
 
 import android.app.Application;
 
 public class SocialApplication extends Application {
 
 	private HttpClient httpClient;
+	private ImageLoader imageLoader;
 
 	@Override
 	public void onCreate() {
 		httpClient = new HttpClient();
+		imageLoader = new ImageLoader();
 	}
 
 	@Override
@@ -18,7 +21,9 @@ public class SocialApplication extends Application {
 		if (name.equals(HttpClient.HTTP_CLIENT)) {
 			return httpClient;
 		}
-		
+		if (name.equals(ImageLoader.IMAGE_LOADER_SERVICE)) {
+			return imageLoader;
+		}
 		return super.getSystemService(name);
 	}
 
