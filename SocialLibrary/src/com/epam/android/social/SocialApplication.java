146cc991e373fe3e1df6053cfb7c465d6e6d25e4
ilya.shknaj <ilya.shknaj@258f82a4-f076-c894-e823-74c26278488b>
2012-02-09 09:13:41 +0000

    added loader to SocialApplication

--- SocialLibrary/src/com/epam/android/social/SocialApplication.java
+++ SocialLibrary/src/com/epam/android/social/SocialApplication.java
@@ -1,6 +1,7 @@
 package com.epam.android.social;
 
 import com.epam.android.common.http.HttpClient;
+import com.epam.android.common.model.Loader;
 import com.google.android.imageloader.ImageLoader;
 
 import android.app.Application;
@@ -8,11 +9,13 @@ import android.app.Application;
 public class SocialApplication extends Application {
 
 	private HttpClient httpClient;
+	private Loader loader;
 	private ImageLoader imageLoader;
 
 	@Override
 	public void onCreate() {
 		httpClient = new HttpClient();
+		loader = new Loader(this);
 		imageLoader = new ImageLoader();
 	}
 
@@ -21,9 +24,15 @@ public class SocialApplication extends Application {
 		if (name.equals(HttpClient.HTTP_CLIENT)) {
 			return httpClient;
 		}
+		
 		if (name.equals(ImageLoader.IMAGE_LOADER_SERVICE)) {
 			return imageLoader;
 		}
+		
+		if(name.equals(Loader.LOADER)){
+			return loader;
+		}
+		
 		return super.getSystemService(name);
 	}
 
