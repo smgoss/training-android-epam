e8f4cc507c05794ad4fa4b126d141f8af3cbd757
ilya.shknaj <ilya.shknaj@258f82a4-f076-c894-e823-74c26278488b>
2012-02-11 09:33:03 +0000

    added button xml to json

--- trunk/SocialLibrary/src/com/epam/android/social/MainActivity.java
+++ trunk/SocialLibrary/src/com/epam/android/social/MainActivity.java
@@ -1,12 +1,22 @@
 package com.epam.android.social;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+
+import com.epam.android.common.http.HttpClient;
+import com.epam.android.common.http.Loader;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends Activity {
-	/** Called when the activity is first created. */
+	private static final String TAG = MainActivity.class.getSimpleName();
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,5 +31,11 @@ public class MainActivity extends Activity {
 	public void onArrayModelButtonClick(View view) {
 		Intent intent = new Intent(this, ArrayModelSampleActivity.class);
 		startActivity(intent);
+	}
+	
+	public void onparseXMLtoJSONButtonClick(View view) throws ClientProtocolException, JSONException, parser.JSONException, IOException{
+		Loader loader = new Loader((HttpClient) getApplicationContext().getSystemService(HttpClient.HTTP_CLIENT));
+		Log.d(TAG, loader.createJsonFromXml("http://partners.mtvnservices.com/dextr/partner/wireless/comedy/tds_mobile_app_news/full.xml").toString());
+		
 	}
 }
