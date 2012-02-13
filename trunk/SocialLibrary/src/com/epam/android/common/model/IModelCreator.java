a4f36cbc9dae3fca5d76df4054779c0a606ded40
agidel2012 <agidel2012@258f82a4-f076-c894-e823-74c26278488b>
2012-02-13 14:32:36 +0000

    Some refactoring

--- trunk/SocialLibrary/src/com/epam/android/common/model/IModelCreator.java
+++ trunk/SocialLibrary/src/com/epam/android/common/model/IModelCreator.java
@@ -1,9 +1,5 @@
 package com.epam.android.common.model;
 
-import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public interface IModelCreator<B> {
