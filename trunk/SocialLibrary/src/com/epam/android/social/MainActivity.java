c07c36a45ffee42e38885c9f76ca63fced1359e4
agidel2012 <agidel2012@258f82a4-f076-c894-e823-74c26278488b>
2012-02-15 13:35:35 +0000

    some fixes on sendBroadcast - BroadcastRecievers

--- trunk/SocialLibrary/src/com/epam/android/social/MainActivity.java
+++ trunk/SocialLibrary/src/com/epam/android/social/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		isOnline();
 		
 	}
 
