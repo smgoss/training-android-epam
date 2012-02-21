bca6a9f6baee5ba9689821e245d3f408298981cb
ilya.shknaj <ilya.shknaj@258f82a4-f076-c894-e823-74c26278488b>
2012-02-21 14:14:13 +0000

    added annotation

--- SocialLibrary/src/com/epam/android/common/http/Loader.java
+++ SocialLibrary/src/com/epam/android/common/http/Loader.java
@@ -1,6 +1,8 @@
 package com.epam.android.common.http;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
@@ -11,12 +13,16 @@ import org.json.JSONObject;
 import org.json.XML;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.epam.android.common.annotation.Tag;
 import com.epam.android.common.model.IModelCreator;
 import com.epam.android.common.utils.GetSystemService;
 import com.epam.android.common.utils.JsonModelConverter;
 
 public class Loader {
+	
+	
 	public static final String LOADER = "++LOADER+";
 
 	@SuppressWarnings("unused")
@@ -56,11 +62,34 @@ public class Loader {
 		//TODO get @Tag annotation get value,  
 		/*return JsonModelConverter.convertJSONArrayToList(jsonArray,
 				modelCreator);*/
+		
+		
 		return null;
 	}
 
 	
 	public JSONObject createJsonFromXml(String url) throws JSONException, ClientProtocolException, IOException {
+		
 		return XML.toJSONObject(mHttpClient.execute(new HttpGet(url)));
+	}
+	
+	@Tag(key = {"item","item2"})
+	public void testAnnotation(){
+		try {
+			try {
+				Method method = Loader.class.getMethod("testAnnotation", null);
+				Annotation[] annotations = method.getAnnotations();
+				if(annotations.length > 0)
+					for(int i = 0; i < ((Tag)annotations[0]).key().length; i++)
+						Log.d(TAG, "" + ((Tag)annotations[0]).key()[i]);
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 }
