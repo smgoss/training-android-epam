95b4928c80fa88e266e04d0fb4e84934390b1ffa
ilya.shknaj <ilya.shknaj@258f82a4-f076-c894-e823-74c26278488b>
2012-02-22 12:19:04 +0000

    fix annotation

--- SocialLibrary/src/com/epam/android/common/annotation/Tag.java
+++ SocialLibrary/src/com/epam/android/common/annotation/Tag.java
@@ -3,8 +3,10 @@ package com.epam.android.common.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import com.epam.android.common.model.JSON;
+
 @Retention(RetentionPolicy.RUNTIME) 
 public @interface Tag {
 	String[] keys();
-	String[] types();
+	JSON [] types();
 }
--- SocialLibrary/src/com/epam/android/social/MainActivity.java
+++ SocialLibrary/src/com/epam/android/social/MainActivity.java
@@ -3,9 +3,11 @@ package com.epam.android.social;
 import java.io.IOException;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
@@ -13,12 +15,14 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.epam.android.common.http.Loader;
 import com.epam.android.common.annotation.Tag;
 import com.epam.android.social.model.Item;
+import com.epam.android.social.model.User;
 
 public class MainActivity extends Activity {
 	
@@ -45,29 +49,34 @@ public class MainActivity extends Activity {
 
 	public void onParseXMLtoJSONButtonClick(View view)
 			throws ClientProtocolException, JSONException, IOException {
-		// Loader loader = new Loader((HttpClient)
-		// getApplicationContext().getSystemService(HttpClient.HTTP_CLIENT));
-	
-		
-		
-		/*Loader loader = Loader.get(this);
-		@Tag(key="item")
-		JSONObject  jsonObject = loader.createJsonFromXml("http://partners.mtvnservices.com/dextr/partner/wireless/daily_show_most_popular_videos_changed/full.xml");
-		Tag tag = Item.class.getAnnotation(Tag.class);
-		String[] key = tag.key();
-		
-		
-		for (int i = 0; i < item.getCategoryLength(0); i++) {
-			Log.d(TAG, "" + item.getPubDate(i));
-			Log.d(TAG, item.getDescription(i));
-			Log.d(TAG, item.getGuid(i));
-			Log.d(TAG, "" + item.getGuidIsPermaLink(i));
-		}
-		
-		jsonObject.getJso(key)
 		Loader loader = Loader.get(this);
-		loader.testAnnotation();
-		*/
+		loader.createJsonFromXml("http://partners.mtvnservices.com/dextr/partner/wireless/daily_show_most_popular_videos_changed/full.xml");
+//		List<Item> item = loader
+//				.loadArrayModelFromXmlByAnnotation(
+//						"http://partners.mtvnservices.com/dextr/partner/wireless/daily_show_most_popular_videos_changed/full.xml",
+//						Item.MODEL_CREATOR);
+		
+//		for(int i = 0; i < item.size(); i++){
+//			Log.d(TAG, item.get(i).getCopiright());
+//			Log.d(TAG, item.get(i).getDescription());
+//			Log.d(TAG, item.get(i).getGuid());
+//			Log.d(TAG, item.get(i).getKeywords());
+//			Log.d(TAG, item.get(i).getLink());
+//			Log.d(TAG, item.get(i).getMedia());
+//			Log.d(TAG, item.get(i).getMediaDuration());
+//			Log.d(TAG, item.get(i).getMediaTitle());
+//			Log.d(TAG, item.get(i).getThumbnail());
+//			Log.d(TAG, item.get(i).getTitle());
+//			Log.d(TAG, "" + item.get(i).getCategoryLength());
+//			Log.d(TAG, "" + item.get(i).getGuidIsPermaLink());
+//			Log.d(TAG, "" + item.get(i).getPubDate());
+//			for (int j = 0; j < item.get(i).getCategoryLength(); j++) {
+//				Log.d(TAG, "i=" + i + " j=" + j);
+//				Log.d(TAG, item.get(i).getCategory(j));
+//				Log.d(TAG, item.get(i).getCategorySchema(j));
+//			}
+//		}
+//		
 	}
 
 	public void onRunAsynkTaskButtonClick(View view) {
