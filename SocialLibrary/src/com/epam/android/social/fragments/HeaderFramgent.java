70f274bd3e97c050f3c9397decc2c9a843f74715
ilya.shknaj <ilya.shknaj@258f82a4-f076-c894-e823-74c26278488b>
2012-04-11 17:44:53 +0000

    added icons

--- SocialLibrary/src/com/epam/android/social/fragments/HeaderFramgent.java
+++ SocialLibrary/src/com/epam/android/social/fragments/HeaderFramgent.java
@@ -7,15 +7,15 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.epam.android.social.R;
 
-public class HeaderFramgent extends Fragment{
+public class HeaderFramgent extends Fragment {
 
 	private static final String TAG = HeaderFramgent.class.getSimpleName();
-	
-	private Button tweetButton;
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -25,20 +25,48 @@ public class HeaderFramgent extends Fragment{
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+
+		getView().findViewById(R.id.tweetButton).setOnClickListener(
+				new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						AddTweetFragment addTweetFragment = new AddTweetFragment();
+						addTweetFragment.show(getFragmentManager(), TAG);
+					}
+				});
+		getView().findViewById(R.id.searchButton).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getView().getContext(), "onSearhcButtonClick", Toast.LENGTH_SHORT).show();
+			}
+		});
 		
-		if(tweetButton == null){
-			tweetButton = (Button) getView().findViewById(R.id.tweetButton);
-			tweetButton.setOnClickListener(new OnClickListener() {
+		getView().findViewById(R.id.homeButton).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getView().getContext(), "onHomeButtonClick", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		getView().findViewById(R.id.favoriteButton).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getView().getContext(), "onFavoriteButtonClick", Toast.LENGTH_SHORT).show();
 				
-				@Override
-				public void onClick(View v) {
-					AddTweetFragment addTweetFragment = new AddTweetFragment();
-					addTweetFragment.show(getFragmentManager(), TAG);
-				}
-			});
-		}
+			}
+		});
+		
+		getView().findViewById(R.id.messagesButton).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getView().getContext(), "onMessageButtonClick", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
-	
-	
 
 }
