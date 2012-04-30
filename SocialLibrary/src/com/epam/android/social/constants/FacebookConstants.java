c51fc33154d43eae3b15f08446198ae6834a9f82
bandysik <bandysik@gmail.com>
2012-04-30 14:26:22 +0000

    added Facebook

---
+++ SocialLibrary/src/com/epam/android/social/constants/FacebookConstants.java
@@ -0,0 +1,15 @@
+package com.epam.android.social.constants;
+
+public class FacebookConstants {
+
+	public static final String APP_ID = "163500670443120";
+	
+	public static final String TOKEN_SECRET = "token_secret";
+
+	public static final String TOKEN = "token";
+	
+	public static final String OAUTH_VERIFIER = "oauth_verifier=";
+	
+	public static final int MAX_LENGTH_TWEET = 140;
+
+}
---
+++ SocialLibrary/src/com/epam/android/social/model/FacebookProfileInfo.java
@@ -0,0 +1,76 @@
+package com.epam.android.social.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+import com.epam.android.common.model.BaseModel;
+import com.epam.android.common.model.IModelCreator;
+
+public class FacebookProfileInfo extends BaseModel {
+
+//	private final String 
+	private static final String TAG = FacebookProfileInfo.class.getSimpleName();
+	
+	public static final IModelCreator<FacebookProfileInfo> MODEL_CREATOR = new IModelCreator<FacebookProfileInfo>() {
+
+		public FacebookProfileInfo create(JSONObject jsonObject) {
+			return new FacebookProfileInfo(jsonObject);
+		}
+
+	};
+
+	public static final Parcelable.Creator<FacebookProfileInfo> CREATOR = new Creator<FacebookProfileInfo>() {
+
+		public FacebookProfileInfo[] newArray(int size) {
+			return new FacebookProfileInfo[size];
+		}
+
+		public FacebookProfileInfo createFromParcel(Parcel in) {
+			return new FacebookProfileInfo(in);
+		}
+	};
+
+	public FacebookProfileInfo() {
+		super();
+	}
+
+	public FacebookProfileInfo(JSONObject json) {
+		super(json);
+	}
+
+	public FacebookProfileInfo(Parcel in) {
+		super(in);
+	}
+
+	public FacebookProfileInfo(String json) {
+		super(json);
+	}
+
+//	public String getProfileAvatarUrl() {
+//		return getString("profile_image_url_https");
+//	}
+
+	public String getUrl() {
+		return getString("link");
+	}
+
+	public String getName() {
+		return getString("name");
+	}
+
+	
+	public String getLocation(){
+		try {
+			return getJSONObject().getJSONObject("location").getString("name");
+		} catch (JSONException e) {
+			Log.e(TAG, "error on gson when get User Name", e);
+		}
+		return "";
+		
+	}
+
+}
