3d5b96e049384c99c0d8b2f61a1852dbc5944716
bandysik <bandysik@gmail.com>
2012-05-10 09:38:32 +0000

    Facebook Load Status ))

--- SocialLibrary/src/com/epam/android/social/facebook/ArrayModelListAdapter.java
+++ SocialLibrary/src/com/epam/android/social/facebook/ArrayModelListAdapter.java
@@ -1,6 +1,11 @@
 package com.epam.android.social.facebook;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import android.content.Context;
 import android.view.View;
@@ -8,6 +13,7 @@ import android.widget.TextView;
 
 import com.epam.android.common.adapter.AbstractAdapter;
 import com.epam.android.social.R;
+import com.epam.android.social.helper.DataConvertHelper;
 import com.epam.android.social.model.StatusFacebook;
 
 public class ArrayModelListAdapter extends AbstractAdapter<StatusFacebook> {
@@ -21,8 +27,32 @@ public class ArrayModelListAdapter extends AbstractAdapter<StatusFacebook> {
 
 	@Override
 	public void init(View view, StatusFacebook item) {
+
+		// ImageView userAvatar = (ImageView)
+		// view.findViewById(R.id.userAvatar);
+		// mImageLoader.setAvatar(item.getProfileUrl(), userAvatar);
 		TextView userName = (TextView) view.findViewById(R.id.userName);
-		userName.setText(item.getName());
+		userName.setText(item.getFromName());
+
+		// Date date = new Date(item.getUpdatedTime());
+		// Date dateNow = Calendar.getInstance().getTime();
+
+		SimpleDateFormat ISO8601DATEFORMAT = new SimpleDateFormat(
+				"yyyy-MM-dd'T'HH:mm:ssZ", Locale.GERMANY);
+		// System.out.println(ISO8601DATEFORMAT.parse(item.getUpdatedTime()));
+
+		TextView datePublic = (TextView) view.findViewById(R.id.postDate);
+		try {
+			datePublic.setText(ISO8601DATEFORMAT.parse(item.getUpdatedTime()).toLocaleString());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// datePublic.setText(DataConvertHelper.getFormattedDate(dateNow,
+		// date));
+
+		TextView tweetText = (TextView) view.findViewById(R.id.postText);
+		tweetText.setText(item.getMessage());
 	}
 
 }
--- SocialLibrary/src/com/epam/android/social/facebook/ListStatusesActivity.java
+++ SocialLibrary/src/com/epam/android/social/facebook/ListStatusesActivity.java
@@ -3,16 +3,15 @@ package com.epam.android.social.facebook;
 import java.util.List;
 
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.epam.android.common.BaseModelActivity;
+import com.epam.android.common.BaseArrayModelByAnnotationActivity;
 import com.epam.android.social.R;
 import com.epam.android.social.api.FacebookAPI;
 import com.epam.android.social.constants.ApplicationConstants;
 import com.epam.android.social.model.StatusFacebook;
 
 public class ListStatusesActivity extends
-		BaseModelActivity<StatusFacebook> {
+		BaseArrayModelByAnnotationActivity<StatusFacebook> {
 
 	private static final String TAG = ListStatusesActivity.class
 			.getSimpleName();
@@ -23,12 +22,10 @@ public class ListStatusesActivity extends
 
 	@Override
 	public String getUrl() {
-		// return "http://dl.dropbox.com/u/16403954/array_bm.json";
-		return FacebookAPI.getInstance().getUser()
+		return FacebookAPI.getInstance().getStatuses()
 				+ getIntent().getStringExtra(
 						ApplicationConstants.ARG_PROFILE_NAME);
 	}
-
 
 	@Override
 	public int getLayoutResource() {
@@ -36,13 +33,12 @@ public class ListStatusesActivity extends
 	}
 
 	@Override
-	protected void success(StatusFacebook result) {
+	protected void success(List<StatusFacebook> result) {
 		mListView = (ListView) findViewById(R.id.lvStatuses);
-		Toast.makeText(ListStatusesActivity.this, result.getName(), 1).show();
-//		adapter = new ArrayModelListAdapter(ListStatusesActivity.this,
-//				R.layout.load_model, result);
-//		mListView.setAdapter(adapter);
-		
+		adapter = new ArrayModelListAdapter(ListStatusesActivity.this,
+				R.layout.load_model, result);
+		mListView.setAdapter(adapter);
+
 	}
 
 }
