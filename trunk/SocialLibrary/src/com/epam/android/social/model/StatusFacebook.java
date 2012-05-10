4ac1d08384eab5dc5475f91aa8c5aa640aea97c3
bandysik@gmail.com <bandysik@gmail.com@258f82a4-f076-c894-e823-74c26278488b>
2012-05-10 09:38:32 +0000

    Facebook Load Status ))

--- trunk/SocialLibrary/src/com/epam/android/social/model/StatusFacebook.java
+++ trunk/SocialLibrary/src/com/epam/android/social/model/StatusFacebook.java
@@ -6,9 +6,12 @@ import org.json.JSONObject;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.epam.android.common.annotation.JSON;
+import com.epam.android.common.annotation.Tag;
 import com.epam.android.common.model.BaseModel;
 import com.epam.android.common.model.IModelCreator;
 
+@Tag(keys = { "data", "paging" }, types = { JSON.JSONArray, JSON.JSONObject })
 public class StatusFacebook extends BaseModel {
 
 	@SuppressWarnings("unused")
@@ -49,11 +52,72 @@ public class StatusFacebook extends BaseModel {
 		super(json);
 	}
 
-	public String getName() {
-		return getString("name");
+	public String getId() {
+		return getString("id");
 	}
 
-	private JSONObject getJSONData() {
-		return getJSONObject("data");
+	public JSONObject getFrom() {
+		return getJSONObject("from");
+	}
+
+	public String getFromId() {
+		try {
+			return getFrom().getString("id");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	public String getFromName() {
+		try {
+			return getFrom().getString("name");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	public String getMessage() {
+		return getString("message");
+	}
+
+	public JSONObject getPlace() {
+		return getJSONObject("place");
+	}
+
+	public String getPlaceId() {
+		try {
+			return getPlace().getString("id");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	public String getPlaceName() {
+		try {
+			return getPlace().getString("name");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	public String getPlaceLocation() {
+		try {
+			return getPlace().getString("location");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	public String getUpdatedTime() {
+		return getString("updated_time");
+	}
+
+	public String getType() {
+		return getString("type");
 	}
 }
