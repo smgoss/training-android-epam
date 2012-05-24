4b6adbfb8075d3456dead3ad9c77eee9c62fa93e
ilya.shknaj <ilya.shknaj@258f82a4-f076-c894-e823-74c26278488b>
2012-05-24 13:31:52 +0000

    added design

---
+++ trunk/SocialLibrary/src/com/epam/android/social/adapter/ProfileInfoButtonsAdapter.java
@@ -0,0 +1,42 @@
+package com.epam.android.social.adapter;
+
+import java.util.List;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ProfileInfoButtonsAdapter extends BaseAdapter{
+
+	private List<Integer> list;
+	
+	public ProfileInfoButtonsAdapter(){
+		
+	}
+	
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
